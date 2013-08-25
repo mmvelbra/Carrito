859b9579861b049904d1d1559e9a99c0ea72d6b9
Daniel Icardo <icardo.daniel@gmail.com>
2013-08-24 21:31:32 -0300

    Estructura y nombre de los temas. Errores en web.xml

--- src/main/java/ar/edu/uk/CarritoUI.java
+++ src/main/java/ar/edu/uk/CarritoUI.java
@@ -4,8 +4,8 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.GAEVaadinServlet;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
@@ -14,28 +14,26 @@ import com.vaadin.ui.VerticalLayout;
 
 @Theme("carrito")
 @SuppressWarnings("serial")
-public class CarritoUI extends UI
-{
+public class CarritoUI extends UI {
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = CarritoUI.class, widgetset = "ar.edu.uk.CarritoWidgetSet")
+	public static class Servlet extends GAEVaadinServlet {
+	}
 
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = CarritoUI.class, widgetset = "ar.edu.uk.CarritoWidgetSet")
-    public static class Servlet extends VaadinServlet {
-    }
+	@Override
+	protected void init(final VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
 
-    @Override
-    protected void init(final VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
-
-        final Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(final ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
-    }
+		final Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent event) {
+				layout.addComponent(new Label("Thank you for clicking"));
+			}
+		});
+		layout.addComponent(button);
+	}
 
 }
